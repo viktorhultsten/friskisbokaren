45972a397410772af0405d0214999e6fa7508bdc
Viktor Hultsten <viktor.hultsten@gmail.com>
2022-12-03 21:05:58 +0100

    mistakes..

--- backend/booker.js
+++ backend/booker.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') require('dotenv').config()
 const db = require('./db')
 const fs = require('fs')
 const friskis = require('./friskis')
